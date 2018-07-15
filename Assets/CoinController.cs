45d34d0d43ad5390d407da3dd03c8fd066a5b37c
neko34p <neko34p@gmail.com>
2018-07-15 22:58:27 +0900

    StepAsideUnityChanプロジェクト　課題（アイテムの破棄）
    アイテムの破棄を追加。あわせて改行調整。

--- Assets/CoinController.cs
+++ Assets/CoinController.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CoinController : MonoBehaviour {
+public class CoinController : MonoBehaviour{
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         this.transform.Rotate(0, Random.Range(0, 360), 0);
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
         this.transform.Rotate(0, 3, 0);
-	}
+    }
 }
---
+++ Assets/ItemController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemController : MonoBehaviour {
+    private GameObject unitychan;
+
+    // Use this for initialization
+    void Start () {
+        this.unitychan = GameObject.Find("unitychan");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Unityちゃんよりも後ろになったら破棄
+        if (this.transform.position.z < this.unitychan.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
