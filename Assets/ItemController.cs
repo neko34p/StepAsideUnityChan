c9de63404ef72642754165a95708b1dba09da034
neko34p <neko34p@gmail.com>
2018-07-16 22:41:34 +0900

    StepAsideUnityChanプロジェクト　課題（アイテムの破棄）
    破棄の条件をUnityちゃんの通過でなくカメラより後方（画面外）に変更。

--- Assets/ItemController.cs
+++ Assets/ItemController.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ItemController : MonoBehaviour {
-    private GameObject unitychan;
+    private GameObject mainCamera;
 
     // Use this for initialization
     void Start () {
-        this.unitychan = GameObject.Find("unitychan");
+        this.mainCamera = GameObject.Find("Main Camera");
     }
 
     // Update is called once per frame
     void Update () {
-        // Unityちゃんよりも後ろになったら破棄
-        if (this.transform.position.z < this.unitychan.transform.position.z)
+        // Unityちゃんを追いかけているカメラより後ろに行ったら破棄
+        if (this.transform.position.z < this.mainCamera.transform.position.z)
         {
             Destroy(gameObject);
         }
